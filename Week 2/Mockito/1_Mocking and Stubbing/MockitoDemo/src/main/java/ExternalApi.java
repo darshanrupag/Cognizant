9d1bfee0cb6c99a53c61dce2b0d5dd691cdf3c37
darshanrupag <118422691+darshanrupag@users.noreply.github.com>
2025-06-29 23:39:59 +0530

    Add files via upload

---
+++ Week 2/Mockito/1_Mocking and Stubbing/MockitoDemo/src/main/java/ExternalApi.java
@@ -0,0 +1,7 @@
+
+public class ExternalApi {
+    public String getData()
+    {
+        return "Data from external API";
+    }
+}
---
+++ Week 2/Mockito/2_Verifying Interactions/MockitoDemo/src/test/java/MyServiceTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+// import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+        
+ 
+    @Test
+    public void testVerifyInteraction() {
+       
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+
+        
+        verify(mockApi).getData();
+    }
+}
